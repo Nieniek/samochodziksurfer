5b3fe66c333d0d49577cbf9befce563e11cd5df6
Nieniek <mareczekblaszczyk@gmail.com>
2024-12-02 13:25:56 +0100

    poprawne renderowanie sie drogi

--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -6,7 +6,6 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 direction;
-    public float forwardSpeed = 10f;
 
     private int desiredLane = 1; // 0 = lewy, 1 = œrodek, 2 = prawy
     public float laneDistance = 4f; // Odleg³oœæ miêdzy pasami
@@ -18,8 +17,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Ustawienie ruchu do przodu
-        direction.z = forwardSpeed;
+        // Wyzerowanie ruchu w osi Z, aby samochód nie porusza³ siê do przodu
+        direction.z = 0;
 
         // Obs³uga wejœcia gracza do zmiany pasa
         if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -46,9 +45,10 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVector = Vector3.zero;
         moveVector.x = (targetPosition.x - transform.position.x) * 10f; // Interpolacja osi X
         moveVector.y = direction.y; // Uwzglêdnienie grawitacji lub skoków, jeœli istniej¹
-        moveVector.z = direction.z; // Ruch do przodu
 
-       
+        // Brak ruchu w osi Z
+        moveVector.z = 0;
+
         controller.Move(moveVector * Time.fixedDeltaTime);
     }
 }
--- Assets/Script/RoadSpanerScript.cs
+++ Assets/Script/RoadSpanerScript.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class RoadSpanerScript : MonoBehaviour
 {
-    public GameObject RoadPiecePrefab;
     public float RoadMoveSpeed = 15;
-    [SerializeField]
-    public List<GameObject> RoadPieces;
+
+    // Nowa lista prefabrykatów, które bêd¹ losowane
+    public List<GameObject> RoadPrefabs;
+
+    private List<GameObject> RoadPieces;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,24 +26,31 @@ public class RoadSpanerScript : MonoBehaviour
     {
         MoveRoadPieces();
         float distanceToLastPiece = Vector3.Distance(transform.position, RoadPieces.Last().transform.position);
-        if (distanceToLastPiece > 10)
+        if (distanceToLastPiece > 6)
         {
-            Vector3 newSpawnPosition = RoadPieces.Last().transform.position + new Vector3(0, 0, 10);
+            Vector3 newSpawnPosition = RoadPieces.Last().transform.position + new Vector3(0, 0, 6);
             SpawnRoadPiece(newSpawnPosition);
         }
-        if (RoadPieces.Count > 10)
+        if (RoadPieces.Count > 20)
         {
             Destroy(RoadPieces[0]);
             RoadPieces.RemoveAt(0);
         }
     }
+
     void SpawnRoadPiece(Vector3 position)
     {
-        // dodaj losowanie drogi z prefabs
-        GameObject newRoadPiece = Instantiate(RoadPiecePrefab, position, Quaternion.identity);
-        newRoadPiece.transform.parent = transform;
-        RoadPieces.Add(newRoadPiece);
+        // Losuj prefab z listy RoadPrefabs
+        if (RoadPrefabs != null && RoadPrefabs.Count > 0)
+        {
+            int randomIndex = Random.Range(0, RoadPrefabs.Count);
+            GameObject selectedPrefab = RoadPrefabs[randomIndex];
+            GameObject newRoadPiece = Instantiate(selectedPrefab, position, Quaternion.identity);
+            newRoadPiece.transform.parent = transform;
+            RoadPieces.Add(newRoadPiece);
+        }
     }
+
     void MoveRoadPieces()
     {
         foreach (GameObject roadPiece in RoadPieces)
@@ -50,11 +60,12 @@ public class RoadSpanerScript : MonoBehaviour
                 * RoadMoveSpeed;
         }
     }
+
     void WarmUp()
     {
-        for (int i = 0; i > -10; i--)
+        for (int i = 0; i > -20; i--)
         {
-            Vector3 newSpawnPosition = transform.position + new Vector3(0, 0, 10 * i);
+            Vector3 newSpawnPosition = transform.position + new Vector3(0, 0, 6 * i);
             SpawnRoadPiece(newSpawnPosition);
         }
     }
